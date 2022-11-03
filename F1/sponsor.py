750930be8c501545e2d8c71d6c8e0786a98c830a
emy1563 <73967655+emy1563@users.noreply.github.com>
2022-11-03 10:29:53 -0500

    acessing df and cleaning data types

--- F1/sponsor.py
+++ F1/sponsor.py
@@ -2,13 +2,16 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import os
+import team_id
 
 url2 = 'https://en.wikipedia.org/wiki/Formula_One_sponsorship_liveries'
 sponsor_page = requests.get(url2)
 
 soup2 = BeautifulSoup(sponsor_page.content, 'html.parser')
 
-team = soup2.find(id = 'Alfa_Romeo')
+constructor = 'Force_India'
+
+team = soup2.find(id = constructor)
 
 header = team.parent
 
@@ -16,4 +19,12 @@ table = header.find_next_sibling('table')
 
 df = pd.read_html(str(table))
 df = pd.concat(df)
-df.to_csv(r'C:\Users\17735\Desktop\F1 Python\F1\sponsor.csv', index = False)
+
+df['Year'] = df['Year'].replace({"\[(.*?)\]":" "}, regex=True).astype('string')
+df["Main colour(s)"] = df["Main colour(s)"].astype('string')
+df['Additional colour(s)'] = df["Additional colour(s)"].astype('string')
+df["Livery sponsor(s)"] = df["Livery sponsor(s)"].astype('string')
+df["Additional major sponsor(s)"] = df["Additional major sponsor(s)"].astype('string')
+df.set_index('Year', inplace = True)
+
+print(df.Index.dtypes)
